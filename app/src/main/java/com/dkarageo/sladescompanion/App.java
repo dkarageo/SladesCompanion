eef6c5627fe2309bab2b15bc3626559b845358c2
Dimitris Karageorgiou (University Repo) <soulrain@outlook.com>
2019-01-12 19:14:02 +0200

    Created a utility class accessing resources without context.

---
+++ app/src/main/java/com/dkarageo/sladescompanion/App.java
@@ -0,0 +1,20 @@
+package com.dkarageo.sladescompanion;
+
+import android.app.Application;
+import android.content.Context;
+
+
+public class App extends Application {
+
+    private static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = this;
+    }
+
+    public static Context getContext(){
+        return mContext;
+    }
+}
