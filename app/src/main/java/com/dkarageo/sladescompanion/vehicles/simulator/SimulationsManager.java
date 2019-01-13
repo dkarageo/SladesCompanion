1e5404dfa2dea6e2db55c596b65a29e25d29a063
Dimitris Karageorgiou (University Repo) <soulrain@outlook.com>
2019-01-13 22:09:35 +0200

    Added support for global simulations management.

--- app/src/main/java/com/dkarageo/sladescompanion/vehicles/simulator/SimulationsManager.java
+++ app/src/main/java/com/dkarageo/sladescompanion/vehicles/simulator/SimulationsManager.java
@@ -27,9 +27,33 @@ public class SimulationsManager {
         return s;
     }
 
+    public void destroySimulation(Simulation s) {
+        s.stop();
+        mSimulations.remove(s);
+    }
+
     public void terminateAllSimulations() {
         for (Simulation s : mSimulations) {
             s.stop();
         }
+    }
+
+    public int getActiveSimulationsCount() {
+        int activeSims = 0;
+        for (Simulation s : mSimulations) if (s.isRunning()) activeSims++;
+        return activeSims;
+    }
+
+    public int getTotalAverageLatencyForActiveSimulations() {
+        int totalLatency = 0;
+        int totalActives = 0;
+        for (Simulation s : mSimulations) {
+            if (s.isRunning()) {
+                totalActives++;
+                totalLatency += s.getCurrentLatency();
+            }
+        }
+        if (totalActives > 0) return totalLatency / totalActives;
+        else return 0;
     }
 }
